e041cbbd5306264d07de980a64e9f8653710e34c
dabananda <imdmitra@gmail.com>
2025-05-12 10:03:29 +0600

    Category CRUD & Notification with Partial Views

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,103 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Category objCategory = _db.Categories.Find(id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+            return View(objCategory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Category objCategory = _db.Categories.Find(id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+            return View(objCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int id)
+        {
+            Category objCategory = _db.Categories.Find(id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(objCategory);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fruits", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Vegetables", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Meat & Fish", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Cooking Ingrediants", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Dairy", DisplayOrder = 5 }
+            );
+        }
+    }
+}
