f921110a8c8123c234b6c8d4d8bbb63a57110ff0
dabananda <imdmitra@gmail.com>
2025-05-15 09:46:49 +0600

    Configured Foreign key relationship between Category & Product Model

--- AspWebApps.Models/Product.cs
+++ AspWebApps.Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,5 +35,9 @@ namespace AspWebApps.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
