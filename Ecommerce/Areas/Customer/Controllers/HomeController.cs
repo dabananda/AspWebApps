8da45d29da452a757a0c1e71c414928a08a4d99c
dabananda <imdmitra@gmail.com>
2025-05-24 23:36:45 +0600

    Displaying products to homepage

--- Ecommerce/Areas/Customer/Controllers/HomeController.cs
+++ Ecommerce/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using AspWebApps.DataAccess.Repository.IRepository;
 using AspWebApps.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,18 @@ namespace Ecommerce.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(products);
         }
 
         public IActionResult Privacy()
