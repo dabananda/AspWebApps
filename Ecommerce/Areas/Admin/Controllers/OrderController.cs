b9cd9ef046feb55b622bf049240e76850b07347e
dabananda <imdmitra@gmail.com>
2025-05-31 10:41:54 +0600

    Order Management UI

---
+++ Ecommerce/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using AspWebApps.DataAccess.Repository.IRepository;
+using AspWebApps.Models;
+using AspWebApps.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+        #endregion
+    }
+}
---
+++ Ecommerce/wwwroot/js/order.js
@@ -0,0 +1,33 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tableData').DataTable({
+        "ajax": {
+            url: '/admin/order/getall'
+        },
+        "columns": [
+            { data: 'id', "width": '5%' },
+            { data: 'name', "width": '10%' },
+            { data: 'phoneNumber', "width": '20%' },
+            { data: 'applicationUser.email', "width": '15%' },
+            { data: 'orderStatus', "width": '10%' },
+            { data: 'orderTotal', "width": '10%' },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/Admin/Order/details?orderId=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                <i class="bi bi-pencil-square"></i> Details
+                            </a>
+                        </div>`;
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}
